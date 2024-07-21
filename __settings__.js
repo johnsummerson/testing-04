a236bdc8c2847c17cc6f182882e47f1693147c46
John Summerson <johnsummerson@Johns-MacBook-Pro-2.local>
2024-07-21 18:22:15 -0400

    testing 4

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2032121.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 188406695 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/188406663/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/188406664/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/188406662/1/ammo.js', 'preload' : true},
+];
